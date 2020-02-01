8fc5dbb17b7475af2d802e7414f50dee953e8304
Kyle Ranger <zranger2345@gmail.com>
2020-02-01 16:28:05 -0600

    Add testing data to db.py

---
+++ db.py
@@ -0,0 +1,32 @@
+# Code for the database interaction (when we have a DB)
+
+# Replace this when we have the database setup
+db = {
+	1: {
+		'address': '11 Dirt road street',
+		'city': 'San Antonio',
+		'state': 'Texas',
+		'zip': '11111',
+	},
+
+	2: {
+		'address': '22 Rock road street',
+		'city': 'Austin',
+		'state': 'Texas',
+		'zip': '22222',
+	},
+
+	3: {
+		'address': '33 Test Ave.',
+		'city': 'Dallas',
+		'state': 'Texas',
+		'zip': '33333',
+	},
+
+	4: {
+		'address': '444 Test road',
+		'city': 'San Antonio',
+		'state': 'Texas',
+		'zip': '44444',
+	},
+}
