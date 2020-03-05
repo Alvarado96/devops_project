c3d9df5201612394eae1f281c3a523a412e5e449
Kyle Ranger <zranger2345@gmail.com>
2020-03-05 12:11:46 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Very simple script that moves the code to the deployment server
 
 # Makes a zip file
-zip devops.zip *.py *.db static/swagger.json || exit 1
+zip devops.zip *.py *.db static/swagger.yaml || exit 1
 
 # Uses scp to move the zip file to the server
 scp devops.zip ddd233@cs47831.fulgentcorp.com:~/ || exit 1
