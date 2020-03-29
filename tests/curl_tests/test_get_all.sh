1bb3005725cb20b136c2d27e7a64c44b483d953a
Kyle Ranger <zranger2345@gmail.com>
2020-03-29 11:42:07 -0500

    Add curl command to test_get_all.sh

--- tests/curl_tests/test_get_all.sh
+++ tests/curl_tests/test_get_all.sh
@@ -1,2 +1,19 @@
-echo "TESTING - GET - http://$host_name:$port_number/properties"
+url="http://$host_name:$port_number/properties"
+echo "TESTING - GET - $url"
+
+curl -s -X GET "$url" > get_all_output.json
+
+# Check return value of curl command
+ret_code="$?"
+if [ "$ret_code" != 0 ]; then
+	echo "CURL FAILED..."
+	cat get_all_output.json
+	rm get_all_output.json
+	exit $ret_code
+fi
+
+cat get_all_output.json
+rm get_all_output.json
+
+echo ""
 exit 0
