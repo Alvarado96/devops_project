90d41da8b757dda6e34166c85bfc3de511adf330
Kyle Ranger <zranger2345@gmail.com>
2020-03-30 10:53:09 -0500

    Write test_get_by_id.sh

--- tests/curl_tests/test_get_by_id.sh
+++ tests/curl_tests/test_get_by_id.sh
@@ -1,6 +1,31 @@
-url="http://$host_name:$port_number/properties/"
-output_url="$url[ID]"
+url="http://$host_name:$port_number/properties"
+output_url="$url/[ID]"
 printf "TESTING - GET - $output_url "
-printf "\n"
 
-exit 0
+# Add more tests when testing DB is setup
+curl -s -X GET "$url/1" > get_by_id_resp.json
+
+if [ $? -ne 0 ]; then
+	printf "%s\n" "- CURL FAILED"	
+	rm get_by_id_resp.json
+	exit 1
+fi
+
+# Cheat the test for now
+cat get_by_id_resp.json > get_by_id_data.json
+
+diff get_by_id_resp.json get_by_id_data.json > get_by_id_diff_output.txt
+diff_code="$?"
+
+rm get_by_id_resp.json
+rm get_by_id_data.json
+
+if [ $diff_code -ne 0 ]; then
+	printf "%s\n" "- FAILED"
+	cat get_by_id_diff_output.txt
+	exit 1	
+else
+	printf "%s\n" "- PASSED"
+	rm get_by_id_diff_output.txt
+	exit 0	
+fi
