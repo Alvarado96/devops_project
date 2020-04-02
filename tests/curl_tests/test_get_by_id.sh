6d154fd49edc3510adbbd0668f6d307b4cbcc84e
fgd806 <fgd806@my.utsa.edu>
2020-04-02 17:13:29 -0500

    Added test for getting id

--- tests/curl_tests/test_get_by_id.sh
+++ tests/curl_tests/test_get_by_id.sh
@@ -1,1 +1,14 @@
-# test_get_by_id
+# Tests the GET by id properties endpoint: /properties/<string:id>
+source ./curl_util.sh
+
+url="http://$host_name:$port_number/properties/1"
+print_test_info_line "GET" "$url"
+
+curl_http "GET" "$url" "" ""
+if [ $? -ne 0 ]; then
+	handle_curl_error
+	exit 1
+fi
+
+check_resp "get_by_id_data.json"
+exit $?
