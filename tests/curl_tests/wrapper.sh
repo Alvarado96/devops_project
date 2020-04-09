ee44a2269049b01331da548f1dc59883680562ed
Kyle Ranger <zranger2345@gmail.com>
2020-04-09 11:18:35 -0500

    Use correct file path to main.py in wrapper.sh

--- tests/curl_tests/wrapper.sh
+++ tests/curl_tests/wrapper.sh
@@ -1,7 +1,7 @@
 # Runs as part of the pipeline. Start the service locally runs the curl tests
 # and kills the server
 echo "Starting curl tests..."
-python3 main.py localhost 12188 &
+python3 ../../main.py localhost 12188 &
 servicePID=$!
 #bash test_driver.py
 echo "exit 1" > a.sh
@@ -9,5 +9,4 @@ bash a.sh
 test_result=$?
 kill -9 $servicePID
 echo "Service shut down, ending stage..."
-rm a.sh
 exit $test_result
