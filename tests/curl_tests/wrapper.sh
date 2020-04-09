abf09ad188029e9b1f9ee9a201dd1f2a1f4af6ae
Kyle Ranger <zranger2345@gmail.com>
2020-04-09 11:14:08 -0500

    Add wrapper script to run curl tests in pipeline

---
+++ tests/curl_tests/wrapper.sh
@@ -0,0 +1,13 @@
+# Runs as part of the pipeline. Start the service locally runs the curl tests
+# and kills the server
+echo "Starting curl tests..."
+python3 main.py localhost 12188 &
+servicePID=$!
+#bash test_driver.py
+echo "exit 1" > a.sh
+bash a.sh
+test_result=$?
+kill -9 $servicePID
+echo "Service shut down, ending stage..."
+rm a.sh
+exit $test_result
