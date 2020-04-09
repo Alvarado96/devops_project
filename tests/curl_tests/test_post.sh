76e103b4d8d210401954902169b84417f2b98298
fgd806 <fgd806@my.utsa.edu>
2020-04-08 19:30:02 -0500

    Update test_post.sh

--- tests/curl_tests/test_post.sh
+++ tests/curl_tests/test_post.sh
@@ -1,2 +1,13 @@
-echo "TESTING - POST - http://$host_name:$port_number/properties/[ID]"
+source ./curl_util.sh
+
+url="http://$host_name:$port_number/properties"
+print_test_info_line "POST" "$url"
+
+curl_http "POST" "$url" "" '{"address":"777 7th st", "city7":"testing7", "state":"TX", "zip":"77777"}'
+
+if [ $? -ne 0 ]; then
+	handle_curl_error
+	exit 1
+fi
+check_resp "./test_resps/property_added.json"
 exit 0
