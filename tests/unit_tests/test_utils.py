5ffad69ac6781f307a9016dc8418179ed6f352af
Kyle Ranger <zranger2345@gmail.com>
2020-04-11 12:16:21 -0500

    Write unit test for is_integer

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -5,7 +5,11 @@ import utils
 
 class TestUtilFunction(unittest.TestCase):
 	def test_is_integer(self):
-		self.assertEqual(1, 1)
+		for i in range(5):
+			self.assertTrue(utils.is_integer(str(i)))
+
+		self.assertFalse(utils.is_integer("not an integer"))
+		self.assertFalse(utils.is_integer("3.145"))
 
 if __name__ == '__main__':
 	unittest.main()
