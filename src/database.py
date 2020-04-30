c9bbb3a21a79d43e056b3265b147f6811ecbf3b7
rek <zranger2345@gmail.com>
2020-04-30 11:24:26 -0500

    Improve code formatting and documentation

--- src/database.py
+++ src/database.py
@@ -6,6 +6,7 @@ import json
 import os
 import sys
 
+
 # Check that database environment variables are set
 if not os.environ.get('MYSQL_USER'):
     print('No MySQL user name')
@@ -23,6 +24,8 @@ if not os.environ.get('MYSQL_DB'):
     print('No MySQL database name')
     sys.exit(1)
 
+
+# Build connection to MySQL database with credentials
 mydb = mysql.connector.connect(
   host=os.environ.get('MYSQL_HOST'),
   user=os.environ.get('MYSQL_USER'),
@@ -30,13 +33,16 @@ mydb = mysql.connector.connect(
   database=os.environ.get('MYSQL_DB')
 )
 
+
+# Gets all properties from the database
 def select_all_properties():
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM properties")
     myresult = mycursor.fetchall()
     return _to_dict(mycursor, myresult)
 
-# method used to handle inserting a new query entry
+
+# Method used to handle inserting a new query entry
 def insert_property(new_property):
     mycursor = mydb.cursor()
     statement = """INSERT INTO properties(address, state, city, zip) VALUES(%s,%s,%s,%s)"""
@@ -44,7 +50,8 @@ def insert_property(new_property):
     mydb.commit()
     return row_cnt
 
-# method updates an entry in the database
+
+# Method updates an entry in the database
 def update_property(property_id, address, city, state, zip_code):
     mycursor = mydb.cursor()
     row_cnt = 1
@@ -72,17 +79,16 @@ def update_property(property_id, address, city, state, zip_code):
     mydb.commit()
     return str(row_cnt)
 
-# method returns an entry specified by the id in dictionary form
+
+# Method returns an entry specified by the id in dictionary form
 def select_property(property_id):
     mycursor = mydb.cursor()
-
     mycursor.execute("SELECT * FROM properties where id = %s", (property_id,))
-
     myresult = mycursor.fetchall()
-
     return _to_dict(mycursor, myresult)
 
-# method handles the query for deleting an entry specified by the id
+
+# Method handles the query for deleting an entry specified by the id
 def delete_property(property_id):
     mycursor = mydb.cursor()
     statement = """DELETE FROM properties WHERE id=%s"""
@@ -90,14 +96,16 @@ def delete_property(property_id):
     mydb.commit()
     return row_cnt
 
-# converts database entry to dictionary
+
+# Converts database entry to dictionary
 def _to_dict(cursor, rows):
     ls = []
     for row in rows:
         ls.append(_make_dicts(cursor, row))     
     return ls
 
-# returns a list of dictionarys
+
+# Returns a list of dictionarys
 def _make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
                 for idx, value in enumerate(row))
