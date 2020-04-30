75bf558f55df4961d6cfdb38c3926460088e830b
Nehemiah Alvarado <fgd806@my.utsa.edu>
2020-04-29 23:02:53 -0500

    Have all endpoints working without try and except blocks though

--- src/database.py
+++ src/database.py
@@ -2,117 +2,78 @@ from flask import Flask
 from flask_mysqldb import MySQL
 from flask import Blueprint
 from main import app
+import mysql.connector
+import json
 
+mydb = mysql.connector.connect(
+  host="easel2.fulgentcorp.com",
+  user="fgd806",
+  passwd="Evlv3h7MzBF30ay967IS",
+  database="fgd806"
+)
 
-app.config['MYSQL_USER'] = 'fgd806'
-app.config['MYSQL_PASSWORD'] = 'Evlv3h7MzBF30ay967IS'
-app.config['MYSQL_HOST'] = 'easel2.fulgentcorp.com'
-app.config['MYSQL_DB'] = 'fgd806'
-app.config['MYSQL_CURSORCLASSS'] = 'DictCursor'
-
-mysql = MySQL(app)
-
-# method handles the query for getting all entries in the database
 def select_all_properties():
-    cur = None
-    statement = '''SELECT id FROM properties'''
-    try:
-        cur = mysql.connection.cursor()
-        cur.execute(statement)
-        rows = cur.fetchall()
-        print(rows)
-        return _to_dict(cur, rows)
-    except Exception as e:
-        print('ERROR: select_all_property: DB error:', e)
-    except Exception as e:
-        print('ERROR: select_all_property: error:', e)
-    finally:
-        if cur:
-            cur.close()
-    return None
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM properties")
+
+    myresult = mycursor.fetchall()
+
+    return _to_dict(mycursor, myresult)
 
 # method used to handle inserting a new query entry
 def insert_property(new_property):
-    con = None
-    statement = 'INSERT INTO properties(address, state, city, zip) VALUES(?,?, ?,?)'
-    try:
-        con = establish_connection()
-        cur = con.cursor()
-        row_cnt = cur.execute(statement, new_property).rowcount
-        con.commit()
-        return row_cnt
-    except sqlite3.Error as e:
-        print('ERROR: insert_property: DB error:', e)
-    except Exception as e:
-        print('ERROR: insert_property: other error:', e)
-    finally:
-        if con:
-            con.close()
-    return -1
+    mycursor = mydb.cursor()
+    statement = """INSERT INTO properties(address, state, city, zip) VALUES(%s,%s,%s,%s)"""
+    row_cnt = mycursor.execute(statement, new_property)
+    mydb.commit()
+    return row_cnt
 
 # method updates an entry in the database
 def update_property(property_id, address, city, state, zip_code):
-    con = None
-    row_cnt = 0
-    con = establish_connection()
-    cur = con.cursor()
-
+    mycursor = mydb.cursor()
+    row_cnt = 1
+    
     if address:
-        statement = 'UPDATE properties set address=(?) where id=(?)'
-        row_cnt = cur.execute(statement, (address, property_id)).rowcount
+        statement = """UPDATE properties set address=%s where id=%s"""
+        mycursor.execute(statement, (address, property_id,))
+        row_cnt = mycursor.rowcount
 
     if city:
-        statement = 'UPDATE properties set city=(?) where id=(?)'
-        row_cnt = cur.execute(statement, (city, property_id)).rowcount
+        statement = """UPDATE properties set city=%s where id=%s"""
+        mycursor.execute(statement, (city, property_id,))
+        row_cnt = mycursor.rowcount
 
     if state:
-        statement = 'UPDATE properties set state=(?) where id=(?)'
-        row_cnt = cur.execute(statement, (state, property_id)).rowcount
+        statement = """UPDATE properties set state=%s where id=%s"""
+        mycursor.execute(statement, (state, property_id,))
+        row_cnt = mycursor.rowcount
 
     if zip_code:
-        statement = 'UPDATE properties set zip=(?) where id=(?)'
-        row_cnt = cur.execute(statement, (zip_code, property_id)).rowcount
+        statement = """UPDATE properties set zip=%s where id=%s"""
+        mycursor.execute(statement, (zip_code, property_id,))
+        row_cnt = mycursor.rowcount
 
-    con.commit()
-    return row_cnt
+    mydb.commit()
+    return str(row_cnt)
 
 # method returns an entry specified by the id in dictionary form
 def select_property(property_id):
-    con = None
-    statement = 'SELECT * FROM properties WHERE id=?'
-    try:
-        con = establish_connection()
-        cur = con.cursor()
-        cur.execute(statement, (property_id,))
-        row = cur.fetchall()
-        return _to_dict(cur, row)
-    except sqlite3.Error as e:
-        print('ERROR: select_property: DB error:', e)
-    except Exception as e:
-        print('ERROR: select_property: error:', e)
-    finally:
-        if con:
-            con.close()
-    return None
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM properties where id = %s", (property_id,))
+
+    myresult = mycursor.fetchall()
+
+    return _to_dict(mycursor, myresult)
 
 # method handles the query for deleting an entry specified by the id
 def delete_property(property_id):
-    con = None
-    statement = 'DELETE FROM properties WHERE id=?'
-    try:
-        con = establish_connection()
-        cur = con.cursor()
-        row_cnt = cur.execute(statement, (property_id,)).rowcount
-        con.commit()
-        return row_cnt
-    except sqlite3.Error as e:
-        print('ERROR: delete_property: DB error:', e)
-    except Exception as e:
-        print('ERROR: delete_property: error:', e)
-    finally:
-        if con:
-            con.close()
-    return -1
+    mycursor = mydb.cursor()
+    statement = """DELETE FROM properties WHERE id=%s"""
+    row_cnt = mycursor.execute(statement, (property_id,))
+    mydb.commit()
+    return row_cnt
 
 # converts database entry to dictionary
 def _to_dict(cursor, rows):
